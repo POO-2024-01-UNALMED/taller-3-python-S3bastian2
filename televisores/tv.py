f4b845c6e42cc19a036e597cf8f229397198f357
S3bastian2 <159493363+S3bastian2@users.noreply.github.com>
2024-02-26 17:15:53 -0500

    Atributos y métodos de la clase TV
    Se agregaron los atributos y el constructor que define los valores de estos.

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,2 +1,11 @@
 class tv:
     numTV = 0
+
+    def __init__(self, marca, estado):
+        self.__marca = marca
+        self.__canal = 1
+        self.__precio = 500
+        self.__estado = estado
+        self.__volumen = 1
+        self.control = None
+        tv.numTV += 1
