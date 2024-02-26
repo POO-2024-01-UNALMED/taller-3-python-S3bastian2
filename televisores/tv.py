7b2d3dc608359862c758d2342f2b0111c41d19ca
S3bastian2 <159493363+S3bastian2@users.noreply.github.com>
2024-02-26 17:46:25 -0500

    Metodos set y get
    Se crearon los metodos set y get para los atributos de la clase TV y tambien para el contador.

--- televisores/tv.py
+++ televisores/tv.py
@@ -15,3 +15,42 @@ class TV:
 
     def getMarca(self):
         return self.__marca
+    
+    def setCanal(self, canal):
+        self.__canal = canal
+
+    def getCanal(self):
+        return self.__canal
+    
+    def setPrecio(self, precio):
+        self.__precio = precio
+    
+    def getPrecio(self):
+        return self.__precio
+    
+    def setVolumen(self, volumen):
+        self.__volumen = volumen
+
+    def getVolumen(self):
+        return self.__volumen
+    
+    def setControl(self, control):
+        self.__control = control
+
+    def getControl(self):
+        return self.control 
+    
+    def turnOn(self):
+        self.__estado = True
+    
+    def turnOff(self):
+        self.__estado = False
+    
+    def getEstado(self):
+        return self.__estado
+    
+    def getNumTV(cls):
+        return cls.numTV
+    
+    def setNumTV(cls, num):
+        cls.numTV = num
