d4cc17fb2072f751e77a76360711f93fc6203fd4
S3bastian2 <159493363+S3bastian2@users.noreply.github.com>
2024-02-26 17:02:25 -0500

    Creación de la clase Marca
    Se ha creado la clase Marca en la cual se definen el atributo nombre, el constructor y los métodos set y get.

---
+++ televisores/marca.py
@@ -0,0 +1,9 @@
+class marca:
+    def __init__(self, nombre):
+        self.__nombre = nombre
+    
+    def getNombre(self):
+        return self.__nombre
+    
+    def setNombre(self, nombre):
+        self.__nombre = nombre
